7c6ea81b2938e9d104025e024e1bf2aaf785d7ca
Torabi Mohammadmoein <mohammadmoein.torabi@volvo.com>
2022-09-04 11:56:55 +0200

    updated architecture

--- tf/azure/ServiceBus.tf
+++
@@ -1,7 +0,0 @@
-
-/* resource "azurerm_servicebus_namespace" "ServiceBus" {
-  name                = "moein-servicebus-namespace-${var.env}"
-  location            = azurerm_resource_group.TerraformingAzureRg.location
-  resource_group_name = azurerm_resource_group.TerraformingAzureRg.name
-  sku                 = "Basic"
-} */
